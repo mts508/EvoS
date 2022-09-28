b6aede6b48b430da0ce9d99dcd949865767819d8
mts508 <mathyasrodriguez@gmail.com>
2022-09-27 23:26:49 -0300

    Freelancerset - added code to return what role is a charactertype

--- EvoS.Framework/Network/Static/FreelancerSet.cs
+++ EvoS.Framework/Network/Static/FreelancerSet.cs
@@ -16,5 +16,65 @@ namespace EvoS.Framework.Network.Static
 
         public static FreelancerSet None = new FreelancerSet() { Types = new List<CharacterType> { CharacterType.None } };
         public static FreelancerSet AllRoles = new FreelancerSet() { Roles = new List<CharacterRole> { CharacterRole.Assassin, CharacterRole.Tank, CharacterRole.Support} };
+
+        public bool Matches(CharacterType characterType)
+        {
+            foreach (CharacterType type in Types)
+            {
+                if (type == characterType) return true;
+            }
+
+            foreach (CharacterRole role in Roles)
+            {
+                if (role == GetRole(characterType)) return true;
+            }
+
+            return false;
+        }
+
+        public CharacterRole GetRole(CharacterType characterType)
+        {
+            switch (characterType)
+            {
+                case CharacterType.Soldier:
+                case CharacterType.Thief:
+                case CharacterType.Blaster:
+                case CharacterType.Gremlins:
+                case CharacterType.Tracker:
+                case CharacterType.Exo:
+                case CharacterType.TeleportingNinja:
+                case CharacterType.Fireborg:
+                case CharacterType.Scoundrel:
+                case CharacterType.Neko:
+                case CharacterType.Sniper:
+                case CharacterType.Trickster:
+                case CharacterType.RobotAnimal:
+                case CharacterType.Samurai:
+                case CharacterType.Iceborg:
+                case CharacterType.BazookaGirl:
+                    return CharacterRole.Assassin;
+                case CharacterType.BattleMonk:
+                case CharacterType.Valkyrie:
+                case CharacterType.SpaceMarine:
+                case CharacterType.Scamp:
+                case CharacterType.Dino:
+                case CharacterType.Manta:
+                case CharacterType.Rampart:
+                case CharacterType.RageBeast:
+                case CharacterType.Claymore:
+                    return CharacterRole.Tank;
+                case CharacterType.DigitalSorceress:
+                case CharacterType.FishMan:
+                case CharacterType.NanoSmith:
+                case CharacterType.Archer:
+                case CharacterType.Cleric:
+                case CharacterType.Martyr:
+                case CharacterType.Spark:
+                case CharacterType.Sensei:
+                    return CharacterRole.Support;
+                default:
+                    return CharacterRole.None;
+            }
+        }
     }
 }
