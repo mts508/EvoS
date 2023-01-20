11cb99b29d8c4e24b91f4ebf54a841ee20534261
mts508 <mathyasrodriguez@gmail.com>
2023-01-20 08:48:03 -0300

    Add missing AllianceMessage

---
+++ LobbyServer2/BridgeServer/Messages/ServerGameMetricsNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[Serializable]
+public class ServerGameMetricsNotification : AllianceMessageBase
+{
+    public int CurrentTurn;
+
+    public int TeamAPoints;
+
+    public int TeamBPoints;
+
+    public float AverageFrameTime;
+}
+
